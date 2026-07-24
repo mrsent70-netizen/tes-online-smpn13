52ece301d6942ca462825e071e11cc13104d7565
mrsent70-netizen <mrsent70@gmail.com>
2026-07-24 08:48:59 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbyqHeK6aoMR_cLWEZHK_QCEz_A4ngRL81BJDmN5O7W6RKToVlTXD-Q5KsFHVYjIvBTHuQ/exec',
+  NAMA_SEKOLAH: 'SMP NEGERI 13 BANDAR LAMPUNG',
+  NAMA_APLIKASI: 'SISTEM TES ONLINE',
+  LOGO_URL: 'https://i.ibb.co.com/Zz9dbRD/logosmp13.png'
+};
